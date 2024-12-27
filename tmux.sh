8ecb9bdab48b23e25f7372cf150eefcc3006e155
jan146 <anurjan@gmail.com>
2024-12-27 16:55:33 +0100

    Run compose yaml in tmux

--- tmux.sh
+++ tmux.sh
@@ -9,6 +9,9 @@ tmux send-keys -t "$SESSION_NAME" "nvim ." C-m
 tmux split-window -v -l "33%"
 # Run shell with activated venv in lower pane
 tmux send-keys -t "$SESSION_NAME" ". venv/bin/activate" C-m
+# Run database in bottom-right pane
+tmux split-window -h
+tmux send-keys -t "$SESSION_NAME" "podman-compose -f compose.yaml up" C-m
 # Attach to session
 tmux attach -t "$SESSION_NAME"
 
