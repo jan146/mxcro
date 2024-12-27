3f3a6ada5c053d5387199bb5d04c4d2b8d9c45f1
jan146 <anurjan@gmail.com>
2024-12-27 22:38:47 +0100

    Actually, let's start with a different one

--- food_item/src/api/v1/api.py
+++ food_item/src/api/v1/api.py
@@ -7,7 +7,7 @@ app: Flask = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return "Hello, this is a Flask Microservice"
+    return "Hello, this is the root endpoint of food_item"
 
 if __name__ == "__main__":
     environment: str = os.environ.get("ENVIRONMENT", "development").lower()
