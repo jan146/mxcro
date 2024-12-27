d9555d9eebc841e66c73f15e8239faf1bf9186ca
jan146 <anurjan@gmail.com>
2024-12-27 22:39:17 +0100

    Added entity and converter for FoodItem

---
+++ food_item/src/models/converters/food_item_converter.py
@@ -0,0 +1,22 @@
+from typing import Any
+from food_item.src.models.entities.food_item import FoodItem
+
+class FoodItemConverter():
+    @staticmethod
+    def to_entity(content: dict[str, Any]) -> FoodItem:
+        food_item: FoodItem = FoodItem(
+            name=content["name"],
+            calories=content["calories"],
+            serving_size=content["serving_size_g"],
+            fat_total=content["fat_total_g"],
+            fat_saturated=content["fat_saturated_g"],
+            protein=content["protein_g"],
+            carbohydrates=content["carbohydrates_total_g"],
+            fiber=content["fiber_g"],
+            sugar=content["sugar_g"],
+            sodium=content["sodium_mg"],
+            potassium=content["potassium_mg"],
+            cholesterol=content["cholesterol_mg"],
+        )
+        return food_item
+
---
+++ food_item/src/models/entities/food_item.py
@@ -0,0 +1,16 @@
+import mongoengine as me
+
+class FoodItem(me.Document):
+    name: str = me.StringField()
+    calories: float = me.FloatField()
+    serving_size: float = me.FloatField()
+    fat_total: float = me.FloatField()
+    fat_saturated: float = me.FloatField()
+    protein: float = me.FloatField()
+    carbohydrates: float = me.FloatField()
+    fiber: float = me.FloatField()
+    sugar: float = me.FloatField()
+    sodium: float = me.FloatField()
+    potassium: float = me.FloatField()
+    cholesterol: float = me.FloatField()
+
