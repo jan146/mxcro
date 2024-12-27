22fc7fd3e62b41c7dc3472bee9f8c2c1dccfee69
jan146 <anurjan@gmail.com>
2024-12-27 22:55:51 +0100

    Added servlet for user_info

---
+++ user_info/src/api/v1/api.py
@@ -0,0 +1,22 @@
+import requests
+import os
+from flask import Flask, jsonify
+from gevent.pywsgi import WSGIServer
+
+app: Flask = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Hello, this is the root endpoint of user_info"
+
+if __name__ == "__main__":
+    environment: str = os.environ.get("ENVIRONMENT", "development").lower()
+    debug: bool = (os.environ.get("FLASK_DEBUG", "True").lower() != "false")
+    host: str = os.environ.get("FLASK_HOST", "0.0.0.0")
+    port: int = int(os.environ.get("FLASK_PORT", 5000))
+    if environment in ["prod", "production"]:
+        http_server = WSGIServer((host, port), app)
+        http_server.serve_forever()
+    else:
+        app.run(debug=debug, host=host, port=port)
+
