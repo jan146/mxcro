2115fe6c9db0fcf05e2bf43d10201e0b45ed2fc4
jan146 <anurjan@gmail.com>
2024-12-28 17:38:12 +0100

    Open frontend in tmux

--- tmux.sh
+++ tmux.sh
@@ -3,7 +3,9 @@ SESSION_NAME="mxcro"
 
 # Create new session
 tmux new -s "$SESSION_NAME" -x "$(tput cols)" -y "$(tput lines)" -d
-# Open NeoVIM
+
+# First window: backend
+# Open NeoVIM in top pane
 tmux send-keys -t "$SESSION_NAME" "nvim ." C-m
 # Split horizontally (vertically stacked)
 tmux split-window -v -l "33%"
@@ -12,6 +14,18 @@ tmux send-keys -t "$SESSION_NAME" ". venv/bin/activate" C-m
 # Run database in bottom-right pane
 tmux split-window -h
 tmux send-keys -t "$SESSION_NAME" "podman-compose -f compose.yaml up" C-m
+
+# Create new window for frontend
+tmux new-window
+# Open NeoVIM in top pane
+tmux send-keys -t "$SESSION_NAME" "cd mxcro-frontend && nvim ." C-m
+# Split horizontally (vertically stacked)
+tmux split-window -v -l "33%"
+tmux send-keys -t "$SESSION_NAME" "cd mxcro-frontend" C-m
+# Run terminal in bottom-left pane and bun in bottom-right pane
+tmux split-window -h
+tmux send-keys -t "$SESSION_NAME" "cd mxcro-frontend; bun run dev" C-m
+
 # Attach to session
 tmux attach -t "$SESSION_NAME"
 
