c0c1ad3e84e9d3faadd1f6e36c9c265eaeb7367c
jan146 <anurjan@gmail.com>
2024-12-28 21:23:38 +0100

    Filtering by pk is apparently meant to be difficult or something

--- user_info/src/core/user_info.py
+++ user_info/src/core/user_info.py
@@ -3,7 +3,7 @@ from user_info.src.models.entities.user_info import UserInfo
 
 def get_user_info(id: str) -> UserInfo | None:
     try:
-        user_infos: list[UserInfo] | None = UserInfo.objects(id=ObjectId(id))
+        user_infos: list[UserInfo] | None = UserInfo.objects(pk=ObjectId(id))
     except Exception as e:
         return None
     if user_infos:
