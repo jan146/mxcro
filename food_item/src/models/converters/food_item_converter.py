89c6fce75804eb9e3a11fb3bf68c6f7ef99a6a27
jan146 <anurjan@gmail.com>
2024-12-30 23:02:59 +0100

    Made converter more robust (works with field names both from API and native microservice)

--- food_item/src/models/converters/food_item_converter.py
+++ food_item/src/models/converters/food_item_converter.py
@@ -3,24 +3,30 @@ from food_item.src.models.entities.food_item import FoodItem
 
 WEIGHT_DEFAULT: float = 100.0
 
+def get_multiple_keys(dictionary: dict[Any, Any], keys: list[Any]) -> Any:
+    for key in keys:
+        if dictionary.get(key):
+            return dictionary[key]
+    raise KeyError(f"None of the following keys were present: {str(keys)}")
+
 class FoodItemConverter():
     @staticmethod
     def to_entity(content: dict[str, Any], normalized_weight: bool = True) -> FoodItem:
-        weight_g: float = content["serving_size_g"]
+        weight_g: float = get_multiple_keys(content, ["serving_size_g", "weight_g"])
         normalization_multiplier: float = WEIGHT_DEFAULT / weight_g if normalized_weight else 1.0
         food_item_entity: FoodItem = FoodItem(
             name=content["name"].lower(),
             calories=(content["calories"] * normalization_multiplier),
             weight_g=WEIGHT_DEFAULT if normalized_weight else weight_g,
-            fat_total=(content["fat_total_g"] * normalization_multiplier),
-            fat_saturated=(content["fat_saturated_g"] * normalization_multiplier),
-            protein=(content["protein_g"] * normalization_multiplier),
-            carbohydrates=(content["carbohydrates_total_g"] * normalization_multiplier),
-            fiber=(content["fiber_g"] * normalization_multiplier),
-            sugar=(content["sugar_g"] * normalization_multiplier),
-            sodium=(content["sodium_mg"] * normalization_multiplier),
-            potassium=(content["potassium_mg"] * normalization_multiplier),
-            cholesterol=(content["cholesterol_mg"] * normalization_multiplier),
+            fat_total=(get_multiple_keys(content, ["fat_total_g", "fat_total"]) * normalization_multiplier),
+            fat_saturated=(get_multiple_keys(content, ["fat_saturated_g", "fat_saturated"]) * normalization_multiplier),
+            protein=(get_multiple_keys(content, ["protein_g", "protein"]) * normalization_multiplier),
+            carbohydrates=(get_multiple_keys(content, ["carbohydrates_total_g", "carbohydrates"]) * normalization_multiplier),
+            fiber=(get_multiple_keys(content, ["fiber_g", "fiber"]) * normalization_multiplier),
+            sugar=(get_multiple_keys(content, ["sugar_g", "sugar"]) * normalization_multiplier),
+            sodium=(get_multiple_keys(content, ["sodium_mg", "sodium"]) * normalization_multiplier),
+            potassium=(get_multiple_keys(content, ["potassium_mg", "potassium"]) * normalization_multiplier),
+            cholesterol=(get_multiple_keys(content, ["cholesterol_mg", "cholesterol"]) * normalization_multiplier),
         )
         return food_item_entity
     @staticmethod
