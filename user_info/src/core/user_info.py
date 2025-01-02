c3b3a2cf486f727e7f0d4703250a681293a51703
jan146 <anurjan@gmail.com>
2025-01-02 13:14:23 +0100

    Minor change

--- user_info/src/core/user_info.py
+++ user_info/src/core/user_info.py
@@ -3,7 +3,7 @@ from user_info.src.models.entities.user_info import UserInfo
 
 def get_user_info(id: str) -> UserInfo | None:
     try:
-        user_infos: list[UserInfo] | None = UserInfo.objects(pk=ObjectId(id))
+        user_infos: list[UserInfo] | None = list(UserInfo.objects(pk=ObjectId(id)))
     except Exception as e:
         return None
     if user_infos:
@@ -13,7 +13,7 @@ def get_user_info(id: str) -> UserInfo | None:
 
 def get_user_info_by_username(username: str) -> UserInfo | None:
     try:
-        user_infos: list[UserInfo] | None = UserInfo.objects(username=username)
+        user_infos: list[UserInfo] | None = list(UserInfo.objects(username=username))
     except Exception as e:
         return None
     if user_infos:
