e758d6d64ec4821a2a1084c971f3b71ddfb09cbf
jan146 <anurjan@gmail.com>
2025-01-04 13:42:45 +0100

    Added gender & act. level to UserInfo entity

--- user_info/src/models/entities/user_info.py
+++ user_info/src/models/entities/user_info.py
@@ -1,8 +1,23 @@
+from enum import Enum
 import mongoengine as me
+
+class Gender(Enum):
+    MALE = "m"
+    FEMALE = "f"
+
+class ActivityLevel(Enum):
+    SEDENTARY = "sedentary"
+    LIGHT = "light"
+    MODERATE = "moderate"
+    HIGH = "high"
+    VERY_HIGH = "very_high"
+    EXTREME = "extreme"
 
 class UserInfo(me.Document):
     username: str = me.StringField(required=True, unique=True)
     age: int = me.IntField()
     height: float = me.FloatField()
     weight: float = me.FloatField()
+    gender: Gender = me.EnumField(Gender)
+    activity_level: ActivityLevel = me.EnumField(ActivityLevel)
 
